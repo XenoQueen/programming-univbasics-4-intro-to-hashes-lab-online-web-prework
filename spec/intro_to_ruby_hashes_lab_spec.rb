9fb7f2f0f32dc9523de9c044b92b52456fc16b1b
Maika Kailani <queenmab051@gmail.com>
2019-10-25 01:44:32 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -44,7 +44,7 @@ describe "working with hashes" do
 
     describe "my_hash_creator" do
       my_hash_creator = {
-        :name => "Grace Hopper", 
+        :name => "Grace Hopper"
       }
       it "accepts a key and a value as parameters and returns a hash with this key/value pair inside" do
         expect(my_hash_creator(:name, 'Grace Hopper')).to be_a(Hash)
