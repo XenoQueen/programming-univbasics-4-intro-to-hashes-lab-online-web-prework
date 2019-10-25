e9c7c2e57954cecb7db436ddb53791487c15fb02
Maika Kailani <queenmab051@gmail.com>
2019-10-25 01:24:00 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -13,7 +13,7 @@ describe "working with hashes" do
 
     describe "my_hash" do
       my_hash = {
-        
+        :boardgame => "Ascension:, "
       }
       it "creates and returns a valid hash that contains key/value pairs of your choice" do
         expect(my_hash).to be_a(Hash)
