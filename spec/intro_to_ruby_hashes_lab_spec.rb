bef80099c31e4189e8ae80bbb3052320f89df9dc
Maika Kailani <queenmab051@gmail.com>
2019-10-25 01:21:51 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -12,6 +12,7 @@ describe "working with hashes" do
     end
 
     describe "my_hash" do
+      
       it "creates and returns a valid hash that contains key/value pairs of your choice" do
         expect(my_hash).to be_a(Hash)
         expect(my_hash.keys.count).to_not eq(0)
